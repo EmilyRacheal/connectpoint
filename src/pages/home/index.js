2dc6583de030133dd3dd60dd59908a0a05f4a2a1
daniel2code <danielnwoke20@gmail.com>
2022-08-31 06:57:18 +0100

    ...

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,12 +3,15 @@ import NavBar from "../../component/Navbar";
 import Header from "./Header";
 import Partner from "./Patners";
 import Payments from "./payments";
-import Solutions from "./solutions/index";
-import Newsletter from "./Newsletter";
-import CTA from "./CTA";
-import Testimoniials from "./Testimonials";
-import Youtube from "./Youtube";
-import Services from "./Services";
+import Solutions from "./solutions"
+import PaymentServices from "./Paymentservices"
+import Services from "./Services"
+import Testimonials from "./Testimonials"
+import CTA from "./CTA"
+import NewsLetter from "./Newsletter"
+import Youtube from "./Youtube"
+import Footer from "../../component/Footer"
+
 
 const Index = () => {
   return (
@@ -16,13 +19,15 @@ const Index = () => {
       <NavBar />
       <Header />
       <Partner />
-      <Services />
       <Payments />
       <Solutions />
-      <Newsletter />
-      <CTA />
-      <Testimoniials />
+      <Services />
+      <PaymentServices />
+      <Testimonials />
       <Youtube />
+      <CTA />
+      <NewsLetter />
+      <Footer />
     </>
   );
 };
